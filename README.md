00a4fadc240f5a3846ca75d6aa01072f05e201a5
Matheus Germano <silvaa.matheus112@gmail.com>
2025-04-15 06:03:31 -0300

    root commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Portfólio Acadêmico
+
+Este é um portfólio acadêmico desenvolvido em **Node.js + Express + EJS**, como atividade proposta para a disciplina de Desenvolvimento Web.
+
+## 📚 Tecnologias Utilizadas
+
+- Node.js
+- Express
+- EJS
+- HTML/CSS
+- TailwindCSS
+- Dotenv
+- Nodemailer
+
+## 🚀 Executando o Projeto
+
+1. Instale as dependências:
+
+   ```bash
+   npm install
+   ```
+
+2. Instale o nodemon para desenvolvimento:
+
+   ```bash
+   npm install --save-dev nodemon
+   ```
+
+3. Adicione este script ao seu `package.json`:
+
+   ```json
+   "scripts": {
+     "dev": "nodemon app.js"
+   }
+   ```
+
+4. Renomeie o arquivo `.env-example` para `.env` e preencha as variáveis de ambiente conforme necessário.
+
+5. Inicie o servidor:
+
+   ```bash
+   npm run dev
+   ```
+
+6. Acesse:
+
+   ```
+   http://localhost:3000
+   ```
---
+++ app.js
@@ -0,0 +1,54 @@
+require("dotenv").config(); // Carregar as variáveis do .env
+
+const express = require("express");
+const path = require("path");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+// Configurações do Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rota principal (formulário)
+app.get("/", (req, res) => {
+  res.render("index", { status: null });
+});
+
+app.post("/", async (req, res) => {
+  console.log(req.body); // Adicione isso para verificar o que está sendo enviado
+  const { name, email, subject, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"Nodemailer - Contato" <${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_TO,
+    subject: subject || "Novo Contato do Portfólio",
+    text: `Nome: ${name}\nEmail: ${email}\nMensagem:\n${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    // Envia a resposta JSON indicando sucesso
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    // Envia a resposta JSON indicando erro
+    res.json({ success: false });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Servidor rodando em http://localhost:${PORT}`),
+);
